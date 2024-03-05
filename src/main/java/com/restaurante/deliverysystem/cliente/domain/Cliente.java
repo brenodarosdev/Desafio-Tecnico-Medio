ef2faf82ba7d226287a5374111cf04da2a66fa93
Breno Daros <brenosouzadaros@gmail.com>
2024-03-05 16:33:39 -0300

    implementa dominio de cliente

---
+++ src/main/java/com/restaurante/deliverysystem/cliente/domain/Cliente.java
@@ -0,0 +1,34 @@
+package com.restaurante.deliverysystem.cliente.domain;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Document(collection = "Cliente")
+public class Cliente {
+    @Id
+    private UUID idCliente;
+    @Email
+    @Indexed(unique = true)
+    private String email;
+    private String nomeCompleto;
+    @Size(min = 18)
+    private int idade;
+
+
+    public Cliente(UUID idCliente, String email, String nomeCompleto, int idade) {
+        this.idCliente = idCliente;
+        this.email = email;
+        this.nomeCompleto = nomeCompleto;
+        this.idade = idade;
+    }
+}
