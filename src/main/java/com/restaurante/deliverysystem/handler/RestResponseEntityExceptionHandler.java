abfa063f00049571b49cf87822a150b7e318f0f5
Breno Daros <brenosouzadaros@gmail.com>
2024-03-15 10:55:32 -0300

    implementa tratamento de erros causados por anotacoes de validaacao

--- src/main/java/com/restaurante/deliverysystem/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/restaurante/deliverysystem/handler/RestResponseEntityExceptionHandler.java
@@ -3,8 +3,14 @@ package com.restaurante.deliverysystem.handler;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Log4j2
@@ -18,6 +24,18 @@ public class RestResponseEntityExceptionHandler {
                         .description("INTERNAL SERVER ERROR")
                         .message("POR FAVOR, INFORME AO ADMINISTRADOR DO SISTEMA!")
                         .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 
     @ExceptionHandler(APIException.class)
